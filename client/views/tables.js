6f368fab579a9027d63150d517bc4018a86efc8d
Aram Kocharyan <akarmenia@gmail.com>
2014-12-16 23:18:58 +1100

    More testing with collection table.

--- client/views/tables.js
+++ client/views/tables.js
@@ -7,5 +7,14 @@ Template.tables.helpers({
         label: 'Name'
       }
     ]
+  },
+  colors: function() {
+    var colors = Collections.createTemporary();
+    var i = 0;
+    setInterval(function() {
+      colors.insert({name: 'test' + i});
+      i++;
+    }, 1000);
+    return colors;
   }
 });
